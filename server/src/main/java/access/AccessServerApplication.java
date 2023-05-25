e2bd504c352cd396f4f0a7eee301b343bfd19a47
Okke Harsta <oharsta@zilverline.com>
2023-05-25 16:24:30 +0200

    Use session

--- server/src/main/java/access/AccessServerApplication.java
+++ server/src/main/java/access/AccessServerApplication.java
@@ -3,8 +3,10 @@ package access;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
-@SpringBootApplication(exclude = {SessionAutoConfiguration.class})
+@SpringBootApplication
+@EnableJdbcHttpSession
 public class AccessServerApplication {
 
 	public static void main(String[] args) {
