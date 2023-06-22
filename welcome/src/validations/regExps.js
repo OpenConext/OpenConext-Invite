7f9afe8bec973fd2fc7f37380c6315230c103aba
Okke Harsta <oharsta@zilverline.com>
2023-06-22 11:27:10 +0200

    Fixed JS tests

--- welcome/src/validations/regExps.js
+++ welcome/src/validations/regExps.js
@@ -1,6 +1,7 @@
 export const validUrlRegExp = /(https?|ssh|ftp):\/\/(((www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.?[a-z]{0,63})|(localhost))\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/i
 
 export const constructShortName = name => name.normalize("NFC")
+    .trim()
     .replace(/\s\s+/g, '_')
     .replace(/ /g, '_')
     .replace(/[^A-Za-z0-9_.]/g, '')
