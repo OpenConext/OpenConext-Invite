646d0afe8c5085fda257a97afa60e8bc43d830bb
Okke Harsta <oharsta@zilverline.com>
2023-09-21 17:43:10 +0200

    WIp for new design

---
+++ server/src/test/java/access/manage/LocalManageTest.java
@@ -0,0 +1,20 @@
+package access.manage;
+
+import access.AbstractTest;
+import access.model.Role;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalManageTest extends AbstractTest {
+
+    @Test
+    void deriveRemoteApplications() {
+        List<Role> roles = roleRepository.findAll();
+        assertEquals(6, roles.size());
+        localManage.deriveRemoteApplications(roles);
+        roles.forEach(role -> assertNotNull(role.getApplication().get("id")));
+    }
+}
