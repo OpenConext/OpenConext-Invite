b8640ac2b5aa604e4922455e00cdf8c32d72361c
Okke Harsta <oharsta@zilverline.com>
2023-12-05 13:54:35 +0100

    Fixed migration

--- server/src/main/java/db/mysql/migration/V13_0__migrate_many_to_many_applications.java
+++ server/src/main/java/db/mysql/migration/V13_0__migrate_many_to_many_applications.java
@@ -3,11 +3,16 @@ package db.mysql.migration;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
 
 
 public class V13_0__migrate_many_to_many_applications extends BaseJavaMigration {
@@ -19,16 +24,25 @@ public class V13_0__migrate_many_to_many_applications extends BaseJavaMigration 
             long roleId = rs.getLong("id");
 
             GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+            String manageId = rs.getString("manage_id");
+            String manageType = rs.getString("manage_type");
 
-            String sql = "INSERT INTO `applications` (`manage_id`, `manage_type`) VALUES(?, ?)";
-            jdbcTemplate.update(conn -> {
-                PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-                preparedStatement.setString(1, rs.getString("manage_id"));
-                preparedStatement.setString(2, rs.getString("manage_type"));
-                return preparedStatement;
-            }, generatedKeyHolder);
+            long applicationId;
 
-            long applicationId = generatedKeyHolder.getKey().longValue();
+            List<Map<String, Object>> maps = jdbcTemplate.queryForList("SELECT `id`, `manage_id`, `manage_type` from `applications` WHERE " +
+                    "`manage_id` = ? AND `manage_type` = ?", manageId, manageType);
+            if (maps.isEmpty()) {
+                String sql = "INSERT INTO `applications` (`manage_id`, `manage_type`) VALUES(?, ?)";
+                jdbcTemplate.update(conn -> {
+                    PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                    preparedStatement.setString(1, manageId);
+                    preparedStatement.setString(2, manageType);
+                    return preparedStatement;
+                }, generatedKeyHolder);
+                applicationId = generatedKeyHolder.getKey().longValue();
+            } else {
+                applicationId = (long) maps.get(0).get("id");
+            }
             jdbcTemplate.update("INSERT INTO `roles_applications` (`role_id`,`application_id`) VALUES (?, ?)",
                     roleId, applicationId);
         });
