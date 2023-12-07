e21ed97b791aea1bb2d8580411f3ce9832e57263
Okke Harsta <oharsta@zilverline.com>
2023-12-07 13:21:04 +0100

    Imports

--- server/src/test/java/access/api/APITokenControllerTest.java
+++ server/src/test/java/access/api/APITokenControllerTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
-import static access.AbstractTest.*;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
