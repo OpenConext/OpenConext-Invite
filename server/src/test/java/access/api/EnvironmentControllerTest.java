ea2949044de7ba16016e777076a225a98516ce20
Okke Harsta <oharsta@zilverline.com>
2024-04-28 11:15:27 +0200

    Upped dependencies

--- server/src/test/java/access/api/EnvironmentControllerTest.java
+++ server/src/test/java/access/api/EnvironmentControllerTest.java
@@ -1,18 +1,15 @@
 package access.api;
 
 import access.AbstractTest;
-import access.model.Authority;
-import access.model.Invitation;
-import io.restassured.http.ContentType;
-import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
+import wiremock.org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EnvironmentControllerTest extends AbstractTest {
 
