e626bb0aed458dd1b9d936ac98d17a535d7d102c
Okke Harsta <oharsta@zilverline.com>
2024-05-30 13:06:21 +0200

    Runtime fix for not prefixed passwords

--- server/src/main/java/access/security/ExtendedInMemoryUserDetailsManager.java
+++ server/src/main/java/access/security/ExtendedInMemoryUserDetailsManager.java
@@ -12,10 +12,22 @@ import static java.util.function.Function.identity;
 
 public class ExtendedInMemoryUserDetailsManager implements UserDetailsService {
 
+    private final String noopPrefix = "{noop}";
     private final Map<String, RemoteUser> users;
 
     public ExtendedInMemoryUserDetailsManager(List<RemoteUser> users) {
-        this.users = users.stream().collect(Collectors.toMap(UserDetails::getUsername, identity()));
+        users.forEach(this::fixPassword);
+        this.users = users.stream()
+                .collect(Collectors.toMap(UserDetails::getUsername, identity()));
+    }
+
+    private void fixPassword(RemoteUser remoteUser) {
+        String password = remoteUser.getPassword();
+        boolean hasNoopPrefix = password.startsWith(noopPrefix);
+        if (!hasNoopPrefix) {
+            remoteUser.setPassword(noopPrefix.concat(password));
+        }
+
     }
 
     @Override
