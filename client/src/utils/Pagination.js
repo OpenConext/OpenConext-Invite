f23543f6a44f0bffe3520b270438d1e29fc95aa3
Okke Harsta <oharsta@zilverline.com>
2024-11-26 10:57:08 +0100

    Pagination query params tests

--- client/src/utils/Pagination.js
+++ client/src/utils/Pagination.js
@@ -2,45 +2,6 @@ import {isEmpty} from "./Utils";
 
 export const pageCount = 10;
 
-//https://gist.github.com/kottenator/9d936eb3e4e3c3e02598
-export const pagination = (page, totalResults) => {
-    const delta = 2,
-        left = page - delta,
-        right = page + delta + 1,
-        range = [],
-        rangeWithDots = []
-    let l;
-
-    for (let i = 1; i <= totalResults; i++) {
-        if ((i === 1 || i === totalResults) || (i >= left && i < right)) {
-            range.push(i);
-        }
-    }
-
-    for (const i of range) {
-        if (l) {
-            if (i - l === 2) {
-                rangeWithDots.push(l + 1);
-            } else if (i - l !== 1) {
-                rangeWithDots.push('...');
-            }
-        }
-        rangeWithDots.push(i);
-        l = i;
-    }
-    return rangeWithDots;
-}
-
-export const extractPageResultFromServerResult = (page, sorted, sortDirection) => {
-    return {
-        total: page.totalElements,
-        pageCount: page.pageable.pageSize,
-        currentPage: page.pageable.pageNumber,
-        sort: sorted,
-        sortDirection: sortDirection
-    }
-}
-
 export const paginationQueryParams = (page, queryParams = {}) => {
     if (!isEmpty(page)) {
         if (!isEmpty(page.query)) {
@@ -59,12 +20,10 @@ export const paginationQueryParams = (page, queryParams = {}) => {
             queryParams.sortDirection = page.sortDirection;
         }
     }
-    const queryPart = Object.entries(queryParams).reduce((acc, entry) => {
+    return Object.entries(queryParams).reduce((acc, entry) => {
         acc += `${entry[0]}=${encodeURIComponent(entry[1])}&`
         return acc;
     }, "");
-    return queryPart;
-
 }
 
 export const defaultPagination = (sort= "name", sortDirection= "ASC") => {
