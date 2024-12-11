9792b4646fdb96849ffa4b49eee5497bd64bd22d
Okke Harsta <oharsta@zilverline.com>
2024-12-11 11:29:55 +0100

    Consistency in entities title

--- client/src/tabs/ApplicationUsers.js
+++ client/src/tabs/ApplicationUsers.js
@@ -113,24 +113,19 @@ export const ApplicationUsers = () => {
         },
     ];
 
-    const title = I18n.t(`users.found`, {
-        count: totalElements,
-        plural: I18n.t(`users.${totalElements === 1 ? "singleUser" : "multipleUsers"}`)
-    })
-
-    return (<div className="mod-application-users">
-        {searching && <Loader/>}
-
-        <Entities entities={users}
-                  modelName="users"
-                  defaultSort="name"
-                  columns={columns}
-                  title={title}
-                  inputFocus={true}
-                  totalElements={totalElements}
-                  customSearch={search}
-                  hideTitle={searching}
-                  busy={searching}/>
-    </div>)
+    return (
+        <div className="mod-application-users">
+            {searching && <Loader/>}
+            <Entities entities={users}
+                      modelName="users"
+                      defaultSort="name"
+                      columns={columns}
+                      inputFocus={true}
+                      totalElements={totalElements}
+                      customSearch={search}
+                      hideTitle={searching}
+                      busy={searching}/>
+        </div>
+    );
 
 }
