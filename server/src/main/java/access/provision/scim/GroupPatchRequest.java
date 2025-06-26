9981a5d78f2613770deab710d117573dd93ad28c
Peter Havekes <peter@havekes.eu>
2025-06-26 16:00:28 +0300

    PATCH should not include identifiers

--- server/src/main/java/access/provision/scim/GroupPatchRequest.java
+++ server/src/main/java/access/provision/scim/GroupPatchRequest.java
@@ -11,15 +11,10 @@ import java.util.List;
 public class GroupPatchRequest implements Serializable {
 
     private final List<String> schemas = Collections.singletonList("urn:ietf:params:scim:api:messages:2.0:PatchOp");
-    private final String externalId;
-    private final String id;
-
     @JsonProperty("Operations")
     private final List<Operation> operations;
 
     public GroupPatchRequest(String externalId, String id, Operation operation) {
-        this.externalId = externalId;
-        this.id = id;
         this.operations = Collections.singletonList(operation);
     }
 }
