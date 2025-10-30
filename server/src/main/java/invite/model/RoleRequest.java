aaa4b8f14ef9e74050f55ce3566578c3186a5690
Okke Harsta <oharsta@zilverline.com>
2025-10-30 09:48:09 +0100

    Part of #555

---
+++ server/src/main/java/invite/model/RoleRequest.java
@@ -0,0 +1,46 @@
+package invite.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import invite.provision.scim.GroupURN;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Formula;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RoleRequest implements Serializable{
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    private Integer defaultExpiryDays;
+
+    private boolean enforceEmailEquality;
+
+    private boolean eduIDOnly;
+
+    private boolean blockExpiryDate;
+
+    private boolean overrideSettingsAllowed;
+
+    private String organizationGUID;
+
+    private String inviterDisplayName;
+
+    private Set<ApplicationUsage> applicationUsages = new HashSet<>();
+
+}
