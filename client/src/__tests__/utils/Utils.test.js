4cc94aca1427f52ed7484c1beb124693a6527e93
Leroy <3416288+Liemine@users.noreply.github.com>
2025-11-10 21:46:25 +0100

    259 Migrate test to vitest

--- client/src/__tests__/utils/Utils.test.js
+++ client/src/__tests__/utils/Utils.test.js
@@ -1,21 +1,23 @@
+import { describe, it, expect } from 'vitest';
 import {distinctValues, sanitizeURL} from "../../utils/Utils";
+describe('Utils', () => {
+    it("Test sanitizeURL", () => {
+        expect(sanitizeURL("https://invite.test2.surfconext.nl")).toEqual("https://invite.test2.surfconext.nl");
+    });
 
-test("Test sanitizeURL", () => {
-    expect(sanitizeURL("https://invite.test2.surfconext.nl")).toEqual("https://invite.test2.surfconext.nl");
-});
+    it("Test distinctValues", () => {
+        const res = distinctValues([{id: "1", val: "val1"}, {id: "1", val: "valX"}, {id: "2", val: "val2"}, {
+                id: "3",
+                val: "val3"
+            }],
+            "id");
+        expect(res.length).toEqual(3);
 
-test("Test distinctValues", () => {
-    const res = distinctValues([{id: "1", val: "val1"}, {id: "1", val: "valX"}, {id: "2", val: "val2"}, {
-            id: "3",
-            val: "val3"
-        }],
-        "id");
-    expect(res.length).toEqual(3);
-
-    const resId = distinctValues([{id: 1, val: "val1"}, {id: 1, val: "valX"}, {id: 2, val: "val2"}, {
-            id: 3,
-            val: "val3"
-        }],
-        "id");
-    expect(res.length).toEqual(3);
+        const resId = distinctValues([{id: 1, val: "val1"}, {id: 1, val: "valX"}, {id: 2, val: "val2"}, {
+                id: 3,
+                val: "val3"
+            }],
+            "id");
+        expect(res.length).toEqual(3);
+    });
 });
