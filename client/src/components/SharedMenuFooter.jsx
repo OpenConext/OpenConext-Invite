172800f1a4fff897d7b28b355f02ad52f58c8aee
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2025-11-12 17:15:03 +0100

    #505 Adds footer links in the left menu. Removes old footer

--- client/src/components/SharedMenuFooter.jsx
+++ client/src/components/SharedMenuFooter.jsx
@@ -1,9 +1,29 @@
 import "./SharedMenuFooter.scss";
+import {LanguageSelector} from "./LanguageSelector";
+import I18n from "../locale/I18n";
+import React from "react";
 
 export const SharedMenuFooter = () => {
     return (
-        <div className="shared-menu-footer">
-            <span>NL</span>{' '}|{' '}<span>EN</span>
-        </div>
+        <footer className="shared-menu-footer">
+            <div className="sds--footer--inner">
+                <LanguageSelector />
+                <nav className="menu sds--text--body--small">
+                    <ul>
+                        <li>
+                            <a href={I18n.t("footer.termsLink")} target="_blank"
+                               rel="noopener noreferrer"><span>{I18n.t("footer.terms")}</span></a>
+                        </li>
+                        <li>
+                            <span className="sds--language-sds--divider">|</span>
+                        </li>
+                        <li>
+                            <a href={I18n.t("footer.privacyLink")} target="_blank"
+                               rel="noopener noreferrer"><span>{I18n.t("footer.privacy")}</span></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
     );
 }
