ab7423d30bc52c83a6dcd672ee9019b90bdad732
Leroy <3416288+Liemine@users.noreply.github.com>
2025-11-13 09:55:26 +0100

    #259 Add linter to build.sh

--- client/build.sh
+++ client/build.sh
@@ -4,4 +4,4 @@ rm -Rf target/*
 rm -Rf dist/*
 source $NVM_DIR/nvm.sh
 nvm use
-yarn install --force && CI=true yarn test && yarn build
+yarn install --force && CI=true yarn test && yarn lint && yarn build
