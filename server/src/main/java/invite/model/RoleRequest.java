f0e58afddf65ee2d207d94f60642f228a14cb411
Peter Havekes <peter@havekes.eu>
2026-01-23 10:31:40 +0100

    Add descriptions to RoleRequest for swagger ui

--- server/src/main/java/invite/model/RoleRequest.java
+++ server/src/main/java/invite/model/RoleRequest.java
@@ -2,6 +2,7 @@ package invite.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import invite.provision.scim.GroupURN;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -24,24 +25,33 @@ public class RoleRequest implements Serializable{
 
     @NotNull
     @NotBlank
+    @Schema(description = "Unique name of the role", example = "Guest for my application", required = true)
     private String name;
 
+    @Schema(description = "Brief explanation of this role's purpose", example = "Full access to all modules")
     private String description;
 
+    @Schema(description = "Number of days until the memberschip automaticly expires. Use either defaultExpiryDays or defaultExpiryDate", example = "365")
     private Integer defaultExpiryDays;
 
+    @Schema(description = "Specific timestamp end date for this role membership. Use either defaultExpiryDays or defaultExpiryDate", example = "2026-12-31T23:59:59Z")
     private Instant defaultExpiryDate;
 
+    @Schema(description = "If true, the user's email must match the invitation email", defaultValue = "false")
     private boolean enforceEmailEquality;
 
+    @Schema(description = "The invite can only be accepted by an eduID account", defaultValue = "false")
     private boolean eduIDOnly;
 
     private boolean blockExpiryDate;
 
+    @Schema(description = "Allow changing the settings when sending an invite.", defaultValue = "false")
     private boolean overrideSettingsAllowed;
 
+    @Schema(description = "The unique identifier of the associated organization, only to be used bu SUPER-admins and internal API's", example = "550e8400-e29b-41d4-a716-446655440000")
     private String organizationGUID;
 
+    @Schema(description = "The email address used", example = "550e8400-e29b-41d4-a716-446655440000")
     private String inviterDisplayName;
 
     private Set<ApplicationUsage> applicationUsages = new HashSet<>();
