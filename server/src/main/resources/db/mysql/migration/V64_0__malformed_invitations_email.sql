2b7f3f13b713af3e527a45d9efcae50e91402b94
Okke Harsta <oharsta@gmail.com>
2026-08-25 08:20:40 +0200

    Fixes #804

---
+++ server/src/main/resources/db/mysql/migration/V64_0__malformed_invitations_email.sql
@@ -0,0 +1,7 @@
+UPDATE invitations
+SET email = TRIM(
+        SUBSTRING_INDEX(
+                SUBSTRING_INDEX(email, '<', -1),
+                '>', 1)
+            )
+WHERE email LIKE '%<%@%>%';
